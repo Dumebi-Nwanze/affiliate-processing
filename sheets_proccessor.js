2c3ab982552ff00862925e64a5c8e32ccc7e72b8
Dumebi Nwanze <davisnwanze7@gmail.com>
2023-12-21 16:19:36 +0200

    new log

--- sheets_proccessor.js
+++ sheets_proccessor.js
@@ -209,7 +209,7 @@ async function pushToDialer(
     await fetch(url, options)
       .then(async (response) => {
         if (!response.ok) {
-          const res = "response" + " " + response
+          const res = "response" + " " + response.body
           throw new Error(
             "Error occured while creating lead in dialer CRM:::::::: [" +
               response.status + res+
