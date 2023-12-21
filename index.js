df8eae7c0453c913355e029e84973ea3e65f2044
Dumebi Nwanze <davisnwanze7@gmail.com>
2023-12-21 13:33:25 +0200

    new port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = require('./sheets_proccessor');
 
-const port = process.env.PORT || 80;
+const port =  8080;
 
 
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
