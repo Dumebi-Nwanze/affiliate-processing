3c39c7c12b12f3475159b7ee3ce08a40131a7c49
Dumebi Nwanze <davisnwanze7@gmail.com>
2023-12-21 16:05:54 +0200

    new auth

--- sheets_proccessor.js
+++ sheets_proccessor.js
@@ -159,8 +159,8 @@ async function pushToDialer(
   const url = "https://mc.td.commpeak.com/api/leads";
   const urlmass = "https://mc.td.commpeak.com/api/campaign-leads/mass-assign";
 
-  const username = "developer2";
-  const password = "VDsDVhe3gojb";
+  const username = "developer";
+  const password = "CRnUhSiD2SPU";
   const credentials = btoa(`${username}:${password}`);
 
   const postData = [
