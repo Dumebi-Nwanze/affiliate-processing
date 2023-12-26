1d3346065a7a63126e455e97cdc672a6979b9649
Dumebi Nwanze <davisnwanze7@gmail.com>
2023-12-26 14:04:49 +0200

    new route 9

--- sheets_proccessor.js
+++ sheets_proccessor.js
@@ -805,7 +805,7 @@ app.get("/ftd-clients", verifyToken, async (req, res) => {
           accountUuid: deposit.accountUuid,
           ftd_date: deposit.created,
           amount: deposit.amount,
-          status: response.data.leadStatus,
+          status: response.data.leadStatus.name,
           email: deposit.email,
         };
       })
